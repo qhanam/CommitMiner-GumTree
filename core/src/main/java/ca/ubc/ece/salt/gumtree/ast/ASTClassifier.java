0bd2621382ee618ce27c36b749a62d2b2f836ceb
Quinn Hanam <qhanam@gmail.com>
2019-03-06 10:46:27 -0800

    automated formatting fixes

--- core/src/main/java/ca/ubc/ece/salt/gumtree/ast/ASTClassifier.java
+++ core/src/main/java/ca/ubc/ece/salt/gumtree/ast/ASTClassifier.java
@@ -13,193 +13,211 @@ import ca.ubc.ece.salt.gumtree.ast.ClassifiedASTNode.Version;
 
 public class ASTClassifier {
 
-	/** Assigns uniqueIDs to each node. **/
-	private int uniqueID;
+    /** Assigns uniqueIDs to each node. **/
+    private int uniqueID;
 
-	private ITree srcTree;
-	private ITree dstTree;
+    private ITree srcTree;
+    private ITree dstTree;
 
-	private TreeClassifier treeClassifier;
-	private MappingStore mappings;
+    private TreeClassifier treeClassifier;
+    private MappingStore mappings;
 
-	public ASTClassifier(TreeContext src, TreeContext dst, TreeClassifier treeClassifier, MappingStore mappings) {
+    public ASTClassifier(TreeContext src, TreeContext dst, TreeClassifier treeClassifier,
+	    MappingStore mappings) {
 
-		this.uniqueID = 0;
-		this.srcTree = src.getRoot();
-		this.dstTree = dst.getRoot();
-		this.treeClassifier = treeClassifier;
-		this.mappings = mappings;
+	this.uniqueID = 0;
+	this.srcTree = src.getRoot();
+	this.dstTree = dst.getRoot();
+	this.treeClassifier = treeClassifier;
+	this.mappings = mappings;
 
-	}
+    }
 
-	/**
-	 * Maps Tree node classifications to AST node classifications.
-	 * @throws InvalidClassException
+    /**
+     * Maps Tree node classifications to AST node classifications.
+     * 
+     * @throws InvalidClassException
+     */
+    public void classifyASTNodes() throws InvalidClassException {
+	/* Classify the AST nodes from the Tree nodes. */
+	classifyAs(treeClassifier.getSrcDelTrees(), ChangeType.REMOVED);
+	classifyAs(treeClassifier.getSrcMvTrees(), ChangeType.MOVED);
+	classifyAs(treeClassifier.getSrcUpdTrees(), ChangeType.UPDATED);
+
+	classifyAs(treeClassifier.getDstMvTrees(), ChangeType.MOVED);
+	classifyAs(treeClassifier.getDstUpdTrees(), ChangeType.UPDATED);
+	classifyAs(treeClassifier.getDstAddTrees(), ChangeType.INSERTED);
+
+	/*
+	 * Classify the children of the classified AST nodes and assign node mappings
+	 * for MOVED, UPDATED and UNCHANGED nodes.
 	 */
-	public void classifyASTNodes() throws InvalidClassException {
-		/* Classify the AST nodes from the Tree nodes. */
-		classifyAs(treeClassifier.getSrcDelTrees(), ChangeType.REMOVED);
-		classifyAs(treeClassifier.getSrcMvTrees(), ChangeType.MOVED);
-		classifyAs(treeClassifier.getSrcUpdTrees(), ChangeType.UPDATED);
+	propagateChangesToChildren(srcTree, ChangeType.UNCHANGED, false, true);
+	propagateChangesToChildren(dstTree, ChangeType.UNCHANGED, false, false);
 
-		classifyAs(treeClassifier.getDstMvTrees(), ChangeType.MOVED);
-		classifyAs(treeClassifier.getDstUpdTrees(), ChangeType.UPDATED);
-		classifyAs(treeClassifier.getDstAddTrees(), ChangeType.INSERTED);
+	/* Push changes to ancestors (up to the function level) as UPDATED */
+	propagateChangesToAncestors(srcTree);
+	propagateChangesToAncestors(dstTree);
+    }
 
-		/* Classify the children of the classified  AST nodes and assign
-		 * node mappings for MOVED, UPDATED and UNCHANGED nodes. */
-		propagateChangesToChildren(srcTree, ChangeType.UNCHANGED, false, true);
-		propagateChangesToChildren(dstTree, ChangeType.UNCHANGED, false, false);
-		
-		/* Push changes to ancestors (up to the function level) as UPDATED */
-		propagateChangesToAncestors(srcTree);
-		propagateChangesToAncestors(dstTree);
-	}
+    /**
+     * @return A unique ID for the node.
+     */
+    private int getUniqueID() {
+	this.uniqueID += 16; // Allow for offsetting the unique ID. Yields 2^28 addresses (270M).
+	return this.uniqueID;
+    }
 
-	/**
-	 * @return A unique ID for the node.
+    private void classifyAs(Set<ITree> set, ChangeType changeType) throws InvalidClassException {
+
+	/*
+	 * Iterate through the changed Tree nodes and classify the corresponding AST
+	 * node with the Tree node's class.
 	 */
-	private int getUniqueID() {
-		this.uniqueID += 16; // Allow for offsetting the unique ID. Yields 2^28 addresses (270M).
-		return this.uniqueID;
+	for (ITree tree : set) {
+	    ClassifiedASTNode astNode = tree.getClassifiedASTNode();
+	    switch (changeType) {
+	    case INSERTED:
+	    case REMOVED:
+	    case UPDATED:
+	    case UNCHANGED:
+		astNode.setChangeType(changeType);
+		astNode.setChangeTypeNoProp(changeType);
+		astNode.setMoved(false);
+		break;
+	    case MOVED:
+		astNode.setChangeType(ChangeType.UNCHANGED);
+		astNode.setChangeTypeNoProp(ChangeType.UNCHANGED);
+		astNode.setMoved(true);
+		break;
+	    case UNKNOWN:
+	    case INHERITED:
+	    default:
+		throw new Error("Unsupported change type in AST change type classification.");
+	    }
 	}
-	
-	private void classifyAs(Set<ITree> set, ChangeType changeType) throws InvalidClassException {
 
-		/* Iterate through the changed Tree nodes and classify the
-		 * corresponding AST node with the Tree node's class. */
-		for(ITree tree : set) {
-			ClassifiedASTNode astNode = tree.getClassifiedASTNode();
-			switch(changeType) {
-			case INSERTED:
-			case REMOVED:
-			case UPDATED:
-			case UNCHANGED:
-				astNode.setChangeType(changeType);
-				astNode.setChangeTypeNoProp(changeType);
-				astNode.setMoved(false);
-				break;
-			case MOVED:
-				astNode.setChangeType(ChangeType.UNCHANGED);
-				astNode.setChangeTypeNoProp(ChangeType.UNCHANGED);
-				astNode.setMoved(true);
-				break;
-			case UNKNOWN:
-			case INHERITED:
-			default:
-				throw new Error("Unsupported change type in AST change type classification.");
-			}
-		}
+    }
 
-	}
-	
-	/**
-	 * Propagate the label up to the statement or function level (depending on needs)
+    /**
+     * Propagate the label up to the statement or function level (depending on
+     * needs)
+     */
+    private void propagateChangesToAncestors(ITree subtree) throws InvalidClassException {
+	/*
+	 * Propagate UPDATED label upwards if this subtree was inserted, removed or
+	 * updated.
 	 */
-	private void propagateChangesToAncestors(ITree subtree) throws InvalidClassException {
-		/* Propagate UPDATED label upwards if this subtree was inserted, removed or updated. */
-		switch(subtree.getClassifiedASTNode().getChangeTypeNoProp()) {
-		case INSERTED:
-		case REMOVED:
-		case UPDATED:
-			labelAncestorsUpdated(subtree);
-			break;
-		default:
-		}
-		
-		/* Recursively visit children. */
-		for(ITree childOfSubtree : subtree.getChildren()) 
-			propagateChangesToAncestors(childOfSubtree);
+	switch (subtree.getClassifiedASTNode().getChangeTypeNoProp()) {
+	case INSERTED:
+	case REMOVED:
+	case UPDATED:
+	    labelAncestorsUpdated(subtree);
+	    break;
+	default:
 	}
 
-	/**
-	 * Propagate change labels down to children.
-	 * @param subtree The subtree to label.
-	 * @param ancenstorChangeType The change type of the current node's ancestors.
-	 * @param isSrc {@code true} if the current tree is the original AST; {@code false} if the current tree is the new AST.
-	 * @throws InvalidClassException 
+	/* Recursively visit children. */
+	for (ITree childOfSubtree : subtree.getChildren())
+	    propagateChangesToAncestors(childOfSubtree);
+    }
+
+    /**
+     * Propagate change labels down to children.
+     * 
+     * @param subtree
+     *            The subtree to label.
+     * @param ancenstorChangeType
+     *            The change type of the current node's ancestors.
+     * @param isSrc
+     *            {@code true} if the current tree is the original AST;
+     *            {@code false} if the current tree is the new AST.
+     * @throws InvalidClassException
+     */
+    private void propagateChangesToChildren(ITree subtree, ChangeType ancestorChangeType,
+	    boolean ancestorMoved, boolean isSrc) throws InvalidClassException {
+	ClassifiedASTNode classifiedNode = subtree.getClassifiedASTNode();
+
+	/* This node was moved if its parent was moved. */
+	if (ancestorMoved)
+	    classifiedNode.setMoved(true);
+
+	/* This node assumes the change type of its parent. */
+	if (classifiedNode.getChangeType() == ChangeType.UNKNOWN) {
+	    if (ancestorChangeType != ChangeType.UNCHANGED) {
+		classifiedNode.setChangeType(ancestorChangeType);
+		classifiedNode.setChangeTypeNoProp(ChangeType.INHERITED);
+	    } else {
+		classifiedNode.setChangeType(ChangeType.UNCHANGED);
+		classifiedNode.setChangeTypeNoProp(ChangeType.UNCHANGED);
+	    }
+	}
+
+	/* Assign the node mapping if this is an UPDATED or UNCHANGED node. */
+	if (classifiedNode.getChangeType() == ChangeType.UNCHANGED
+		|| classifiedNode.getChangeType() == ChangeType.UPDATED) {
+	    if (isSrc) {
+		ITree dst = this.mappings.getDst(subtree);
+		if (dst != null)
+		    classifiedNode.map(dst.getClassifiedASTNode());
+	    } else {
+		ITree src = this.mappings.getSrc(subtree);
+		if (src != null)
+		    classifiedNode.map(src.getClassifiedASTNode());
+	    }
+	}
+
+	/* Assign the node a version label. */
+	if (isSrc)
+	    classifiedNode.setVersion(Version.SOURCE);
+	else
+	    classifiedNode.setVersion(Version.DESTINATION);
+
+	/* Classify this node's children with the new change type. */
+	for (ITree childOfSubtree : subtree.getChildren()) {
+	    propagateChangesToChildren(childOfSubtree, classifiedNode.getChangeType(),
+		    classifiedNode.getIsMoved(), isSrc);
+	}
+
+    }
+
+    /**
+     * Visits all the ancestors of the node and labels them as {@code UPDATED} if
+     * they have the label {@code UNCHANGED}.
+     * 
+     * @param child2
+     *            The updated/inserted/removed node who's ancestors should be
+     *            labeled.
+     */
+    private static void labelAncestorsUpdated(ITree child2) throws InvalidClassException {
+
+	/* If this is a function, there is no ancestor to label. */
+	if (child2.getClassifiedASTNode().isFunction())
+	    return;
+
+	/*
+	 * Climb the tree and label all unchanged nodes until we get to the statement.
 	 */
-	private void propagateChangesToChildren(ITree subtree, ChangeType ancestorChangeType, boolean ancestorMoved, boolean isSrc) throws InvalidClassException {
-		ClassifiedASTNode classifiedNode = subtree.getClassifiedASTNode();
+	ITree ancestor = child2.getParent();
+	while (ancestor != null && ancestor.getClassifiedASTNode() != null
+		&& ancestor.getClassifiedASTNode().getChangeType() == ChangeType.UNCHANGED) {
 
-		/* This node was moved if its parent was moved. */
-		if(ancestorMoved) classifiedNode.setMoved(true);
+	    /*
+	     * Label the ancestor, and it's mapped node in the other version, as updated,
+	     * since one of its descendants was inserted, removed or updated.
+	     */
+	    ancestor.getClassifiedASTNode().setChangeType(ChangeType.UPDATED);
+	    ancestor.getClassifiedASTNode().getMapping().setChangeType(ChangeType.UPDATED);
 
-		/* This node assumes the change type of its parent. */
-		if(classifiedNode.getChangeType() == ChangeType.UNKNOWN) {
-			if(ancestorChangeType != ChangeType.UNCHANGED) {
-				classifiedNode.setChangeType(ancestorChangeType);
-				classifiedNode.setChangeTypeNoProp(ChangeType.INHERITED);
-			} else {
-				classifiedNode.setChangeType(ChangeType.UNCHANGED);
-				classifiedNode.setChangeTypeNoProp(ChangeType.UNCHANGED);
-			}
-		}
+	    /* If we've reached the statement level, stop. */
+	    if (ancestor.getClassifiedASTNode().isFunction()) {
+		break;
+	    }
 
-		/* Assign the node mapping if this is an UPDATED or UNCHANGED node. */
-		if(classifiedNode.getChangeType() == ChangeType.UNCHANGED 
-				|| classifiedNode.getChangeType() == ChangeType.UPDATED) {
-			if(isSrc) {
-				ITree dst = this.mappings.getDst(subtree);
-				if(dst != null) classifiedNode.map(dst.getClassifiedASTNode());
-			}
-			else {
-				ITree src = this.mappings.getSrc(subtree);
-				if(src != null) classifiedNode.map(src.getClassifiedASTNode());
-			}
-		}
-		
-		/* Assign the node a unique id if it does not yet have one. */
-		if(classifiedNode.getID() == null) {
-			Integer id = getUniqueID();
-			classifiedNode.setID(id);
-			if(classifiedNode.getMapping() != null)
-				classifiedNode.getMapping().setID(id);
-		}
-
-		/* Assign the node a version label. */
-		if(isSrc) classifiedNode.setVersion(Version.SOURCE);
-		else classifiedNode.setVersion(Version.DESTINATION);
-
-		/* Classify this node's children with the new change type. */
-		for(ITree childOfSubtree : subtree.getChildren()) {
-			propagateChangesToChildren(childOfSubtree, classifiedNode.getChangeType(), classifiedNode.getIsMoved(), isSrc);
-		}
+	    ancestor = ancestor.getParent();
 
 	}
 
-	/**
-	 * Visits all the ancestors of the node and labels them as {@code UPDATED}
-	 * if they have the label {@code UNCHANGED}.
-	 * @param child2 The updated/inserted/removed node who's ancestors should be
-	 * 			   labeled.
-	 */
-	private static void labelAncestorsUpdated(ITree child2) throws InvalidClassException {
-
-		/* If this is a function, there is no ancestor to label. */
-		if(child2.getClassifiedASTNode().isFunction()) return;
-
-		/* Climb the tree and label all unchanged nodes until we get to the
-		 * statement. */
-		ITree ancestor = child2.getParent();
-		while(ancestor != null && ancestor.getClassifiedASTNode() != null &&
-			  ancestor.getClassifiedASTNode().getChangeType() == ChangeType.UNCHANGED) {
-
-			/* Label the ancestor, and it's mapped node in the other version, as
-			 * updated, since one of its descendants was inserted, removed or
-			 * updated. */
-			ancestor.getClassifiedASTNode().setChangeType(ChangeType.UPDATED);
-			ancestor.getClassifiedASTNode().getMapping().setChangeType(ChangeType.UPDATED);
-
-			/* If we've reached the statement level, stop. */
-			if(ancestor.getClassifiedASTNode().isFunction()) {
-				break;
-			}
-
-			ancestor = ancestor.getParent();
-
-		}
-
-	}
+    }
 
 }
--- core/src/main/java/ca/ubc/ece/salt/gumtree/ast/ClassifiedASTNode.java
+++ core/src/main/java/ca/ubc/ece/salt/gumtree/ast/ClassifiedASTNode.java
@@ -4,74 +4,66 @@ import java.io.InvalidClassException;
 
 /**
  * {@link ClassifiedASTNode} is an interface for applying AST differencing
- * classifications (i.e. inserted, removed, moved and updated) to the ASTNode
- * of the base language.
+ * classifications (i.e. inserted, removed, moved and updated) to the ASTNode of
+ * the base language.
  *
- * <p>Because GumTree uses many languages, it produces a common Tree node for
- * each AST node parsed from a language. The tree nodes contain all the
- * information and functionality needed for GumTree, but a lot of language
- * and parser specific features of the AST nodes are lost in the conversion
- * from AST node to Tree node. It is therefore unsuitable to use Tree nodes
- * for CFG production.</p>
+ * <p>
+ * Because GumTree uses many languages, it produces a common Tree node for each
+ * AST node parsed from a language. The tree nodes contain all the information
+ * and functionality needed for GumTree, but a lot of language and parser
+ * specific features of the AST nodes are lost in the conversion from AST node
+ * to Tree node. It is therefore unsuitable to use Tree nodes for CFG
+ * production.
+ * </p>
  *
- * <p>The solution to this problem is to map the change classifications from
- * the Tree nodes back to the AST nodes. ClassifiedASTNode enables this by
- * providing a language-independent interface. To make a specific parser's
- * AST node's classifiable, the AST node class should be modified to implement
- * this interface. For example, the class {@code AstNode} in the Mozilla Rhino
- * parser for JavaScript is modified to support this interface.</p>
+ * <p>
+ * The solution to this problem is to map the change classifications from the
+ * Tree nodes back to the AST nodes. ClassifiedASTNode enables this by providing
+ * a language-independent interface. To make a specific parser's AST node's
+ * classifiable, the AST node class should be modified to implement this
+ * interface. For example, the class {@code AstNode} in the Mozilla Rhino parser
+ * for JavaScript is modified to support this interface.
+ * </p>
  *
  * @author qhanam
  *
  */
 public interface ClassifiedASTNode {
 
-	/**
-	 * @param id A unique ID for the node. Should be the same as the mapped
-	 * 			 node's ID. Used for identifying statements in declarative SA.
-	 */
-	void setID(Integer id);
-
-	/**
-	 * @return The unique ID for the node. Should be the same ID as the mapped
-	 * 		   node's ID. Used for identifying statements in declarative SA.
-	 */
-	Integer getID();
-
-	/**
-	 * @param version The version of the node relative to the commit. Can be
-	 * 				  source or destination node.
-	 */
-	void setVersion(Version version);
-
-	/**
-	 * @return The version of the node relative to the commit. Can be
-	 * 		   source or destination node.
-	 */
-	Version getVersion();
-	
     /**
-     * Indicate that this node provided a dummy value during analysis.
+     * @return The unique ID for the node. Should be the same ID as the mapped
+     *         node's ID. Used for identifying statements in declarative SA.
      */
-	void setDummy();
-	
-	/**
-	 * @return {@code true} if this node provided a dummy value during analysis.
-	 */
-	boolean isDummy();
-	
+    Integer getID();
+
     /**
-     * @param moved If a move was applied to this node from AST differencing.
+     * @param version
+     *            The version of the node relative to the commit. Can be source or
+     *            destination node.
      */
-	void setMoved(boolean moved);
-
-	/**
-	 * @return {@code true} if the node has been moved in the AST (from AST differencing).
-	 */
-	boolean getIsMoved();
+    void setVersion(Version version);
 
     /**
-     * @param changeType The change applied to this node from AST differencing.
+     * @return The version of the node relative to the commit. Can be source or
+     *         destination node.
+     */
+    Version getVersion();
+
+    /**
+     * @param moved
+     *            If a move was applied to this node from AST differencing.
+     */
+    void setMoved(boolean moved);
+
+    /**
+     * @return {@code true} if the node has been moved in the AST (from AST
+     *         differencing).
+     */
+    boolean getIsMoved();
+
+    /**
+     * @param changeType
+     *            The change applied to this node from AST differencing.
      */
     void setChangeType(ChangeType changeType);
 
@@ -81,24 +73,27 @@ public interface ClassifiedASTNode {
     ChangeType getChangeType();
 
     /**
-     * @return The non-propagated edit operation applied to this node from AST differencing.
+     * @return The non-propagated edit operation applied to this node from AST
+     *         differencing.
      */
-	void setChangeTypeNoProp(ChangeType changeTypeNoProp);
+    void setChangeTypeNoProp(ChangeType changeTypeNoProp);
 
     /**
-     * @return The non-propagated edit operation applied to this node from AST differencing.
+     * @return The non-propagated edit operation applied to this node from AST
+     *         differencing.
      */
-	ChangeType getChangeTypeNoProp();
-	
+    ChangeType getChangeTypeNoProp();
+
     /**
-     * @param node The source or destination node to map this node to.
+     * @param node
+     *            The source or destination node to map this node to.
      */
     void map(ClassifiedASTNode node) throws InvalidClassException;
 
     /**
-     * @return The source or destination node that maps to this node or null
-     * 		   if this node does not have a mapping (which is the case for
-     * 		   inserted and removed nodes).
+     * @return The source or destination node that maps to this node or null if this
+     *         node does not have a mapping (which is the case for inserted and
+     *         removed nodes).
      */
     ClassifiedASTNode getMapping();
 
@@ -121,40 +116,35 @@ public interface ClassifiedASTNode {
      * Returns true if this node represents a statement.
      */
     boolean isStatement();
-    
+
     /**
      * Returns true if this node represents a function.
      */
     boolean isFunction();
-    
+
     /**
-     * Set the absolute position of the node in the original (not pre-processed) AST.
+     * Set the absolute position of the node in the original (not pre-processed)
+     * AST.
      */
     void setFixedPosition(int fixedPosition);
-    
+
     /**
-     * @return the absolute position of the node in the original (not pre-processed) AST.
+     * @return the absolute position of the node in the original (not pre-processed)
+     *         AST.
      */
     int getFixedPosition();
 
     /** The change type from AST differencing. **/
     public enum ChangeType {
-    	INSERTED,
-    	REMOVED,
-    	UPDATED,
-    	MOVED,
-    	UNCHANGED,
-    	INHERITED,
-    	UNKNOWN
+	INSERTED, REMOVED, UPDATED, MOVED, UNCHANGED, INHERITED, UNKNOWN
     }
 
     /**
-     * Specifies which version of the change this AstNode is on - the source
-     * (e.g., buggy) or the destination (e.g., repaired).
+     * Specifies which version of the change this AstNode is on - the source (e.g.,
+     * buggy) or the destination (e.g., repaired).
      */
     public enum Version {
-    	SOURCE,
-    	DESTINATION
+	SOURCE, DESTINATION
     }
 
 }
